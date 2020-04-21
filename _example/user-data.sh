b5ef2afeca4512d34c3af5d6ce4a25c50d89eb46
Sohan <sohan@clouddrove.com>
2020-04-21 12:21:13 +0530

    update

--- _example/user-data.sh
+++ _example/user-data.sh
@@ -3,4 +3,4 @@ apt-get update
 apt-get install python
 sudo adduser ubuntu --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 echo "ubuntu ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-usermod -aG sudo username
+usermod -aG sudo ubuntu
